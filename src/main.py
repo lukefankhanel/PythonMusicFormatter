2e39ec58e0ceea950691f42e6c84191a6cdf7f7f
Luke <59628750+lukefankhanel@users.noreply.github.com>
2020-05-13 17:39:24 -0600

    General structure for parsing and continue expansion on editing metadata
    Made some path variables global, allowed for artists translated names to be read from a file, started to parse the lines of required text in the description tag.

--- src/main.py
+++ src/main.py
@@ -5,13 +5,27 @@ import json
 import shutil
 import traceback
 
-#Make sure that the count of songs in the output is the same as the songs in the input
-#Fix issue with the directory loop overwritting files with the same name
-#Delete the old metadata at the end since it's stored in the comment tag
+#Path Names
+STATUS_INFORMATION_FILENAME = "Status Information.json"
+ARTIST_NAME_TRANSLATIONS_FILENAME = "artistnames.json"
+
+#TODO Make sure that the count of songs in the output is the same as the songs in the input
+#TODO Fix issue with the directory loop overwritting files with the same name
+#TODO Delete the old metadata at the end since it's stored in the comment tag
 
 def write_JSON(location, data):
-    with open(location + "/Status Information.json", "w") as f:
+    with open("/".join([location, STATUS_INFORMATION_FILENAME]), "w") as f:
         json.dump(data, f, indent=4)
+
+def access_artist_name_translations():
+    global artist_name_translations
+    artist_name_translations = {}
+    try:
+        with open("/".join([start_location, ARTIST_NAME_TRANSLATIONS_FILENAME])) as f:
+            artist_name_translations = json.load(f)
+        return True
+    except OSError:
+        return False
 
 
 def upper_keys(music_file):
@@ -32,14 +46,62 @@ def create_comment(music_file):
         values.append(" ".join(["KEY=", key, ":", "VALUES=", "".join(values_list)])) # KEY= {} : VALUES= VALUE 1: {} \n
     return "\n".join(values)
 
+
+def parse_line(line):
+    for counter in range(len(line)):
+        if line[counter].isalnum():
+            return line[counter:]
+    raise Exception("Could not find any alphanumeric character in the parsed line.")
+
+def parse_term_line(line, key):
+    if key == "Artist":
+        parsed_line = parse_line(line)
+        for brackets in bracket_types:
+            beginning_bracket_index = parsed_line.find(brackets[0])
+            end_bracket_index = parsed_line.find(brackets[1], beginning_bracket_index)
+            if beginning_bracket_index != -1 and end_bracket_index != -1:
+                parsed_line = parsed_line[beginning_bracket_index + 1 : end_bracket_index]
+            print(parsed_line)
+            #Find out how to check if a character is Japanese
+    
+    #Check the final artist string against the list of artist names and translate it if it's found
+
+    elif key == "Date":
+        pass
+    else:
+        return parse_line(line)
+
+
+def parse_description(description, find_terms):
+    return_dictionary = {}
+
+
+    for key in find_terms.keys():
+        for term in find_terms[key]:
+            if description.find(term) != -1:
+                term_line = (((description.partition(term))[2]).partition("\n"))[0]
+                return_dictionary[key] = parse_term_line(term_line, key)
+                break
+
+    return return_dictionary
+
+
 def change_opus_file(file_path):
     music_file = mutagen.OggOpus(file_path)
 
     upper_keys(music_file)
     music_file["COMMENT"] = create_comment(music_file)
 
-    if "DESCRIPTION" in music_file.keys():
-        pass
+    if "description" in music_file.keys():
+        #Compare this against what is returned
+        find_terms = {
+            "Title": ["Title"],
+            "Album": ["Album"],
+            "Artist": ["Artist", "Circle"],
+            "Date": ["Date"],
+            "Origin": ["Original", "Origin", "Source"]
+        }
+        print(parse_description(music_file["description"][0], find_terms))
     
     music_file.save()
 
@@ -73,10 +135,7 @@ def create_output_folders(location, directory_names):
         pass
 
 
-def access_directory(start_location, output_folder_name):
-
-    output_folder_location = "".join([start_location, "/", output_folder_name])
-
+def access_directory():
     try:
         JSON_data = {}
         JSON_data["statistics"] = {}
@@ -132,10 +191,21 @@ def access_directory(start_location, output_folder_name):
     
 
 def main():
-    start_location = get_music_files_location_input()
-    output_folder_name = get_output_file_name_input()
+    global start_location, output_folder_name, output_folder_location, bracket_types
 
-    if access_directory(start_location, output_folder_name) == 0:
+    start_location = get_music_files_location_input() 
+    output_folder_name = get_output_file_name_input() 
+    output_folder_location = "".join([start_location, "/", output_folder_name])
+    
+    #TODO make the bracket types read from a file
+    bracket_types = [["(", ")"], ["「", "」"], ["【", "】"]]
+
+    if access_artist_name_translations():
+        print("Found Artist Names file!")
+    else:
+        print("Could not find Artist Names file... (artistnames.json)")
+
+    if access_directory() == 0:
         print("Success!")
     else:
         print("Failure")
